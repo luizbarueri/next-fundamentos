7ee05b2842b5e0e29f46b4e1ef684d48a6ceb30e
Luiz Amaro <luizbarueri@gmail.com>
2022-10-24 12:57:28 -0300

    enviando 1 prompt atraves de props, para teste 4

--- src/pages/Cliente/cliente.js
+++ src/pages/Cliente/cliente.js
@@ -2,15 +2,15 @@ import Navegador from '../components/Navegador';
 import CadCliente from './components/CadCliente';
 
 export default function Cliente() {
-   var id0 = prompt("Digite um ID 0:");
-   var id1 = prompt("Digite um ID 1:");
-   var id2 = prompt("Digite um ID 2:");
+   const id = prompt("Digite um ID 0 a 2:");
+   //var id1 = prompt("Digite um ID 1:");
+   //var id2 = prompt("Digite um ID 2:");
     return (
         <div>
             <Navegador />
-            <CadCliente id = { id0 } />
-            <CadCliente id = { id1 } />
-            <CadCliente id = { id2 } />
+            <CadCliente id = {id}/>
+            {/* <CadCliente />
+            <CadCliente /> */}
         </div>
     )
 }
